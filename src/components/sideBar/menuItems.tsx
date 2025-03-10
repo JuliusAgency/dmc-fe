f05e6eeb6f1fc73aaa38b51f080a9235058aba82
jleprog39 <jleprog39@gmail.com>
2025-03-10 10:38:40 +0200

    Saving chanes

--- src/components/sideBar/menuItems.tsx
+++ src/components/sideBar/menuItems.tsx
@@ -3,7 +3,7 @@ import DashboardIcon from "@mui/icons-material/Dashboard";
 import DescriptionIcon from "@mui/icons-material/Description";
 
 export const MENU_ITEMS = [
-  { path: "/home", icon: <HomeIcon />, text: "Home",  disabled: true },
-  { path: "/dashboard", icon: <DashboardIcon />, text: "Dashboard", disabled: false },
-  { path: "/documents", icon: <DescriptionIcon />, text: "Documents", disabled: false },
+  { path: "/home", icon: <HomeIcon />, text: "Home" },
+  { path: "/dashboard", icon: <DashboardIcon />, text: "Dashboard" },
+  { path: "/documents", icon: <DescriptionIcon />, text: "Documents" },
 ];
