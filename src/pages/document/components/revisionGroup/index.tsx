b9398ab7415eeae96afa7eb743bd8598b5816fd1
jleprog39 <jleprog39@gmail.com>
2025-03-10 09:30:17 +0200

    Fix table design

--- src/pages/document/components/revisionGroup/index.tsx
+++ src/pages/document/components/revisionGroup/index.tsx
@@ -1,97 +1,99 @@
-import {Props} from "./types";
-import {Button} from "@mui/material";
-import {GridColDef} from "@mui/x-data-grid-pro";
-import {useGetAllDocuments, useRestoreRevision,} from "../../../../hooks/document/documentHooks.ts";
-import {useCallback, useState} from "react";
-import {PaginationModel} from "../../../../consts/types.ts";
+import { Props } from "./types";
+import { Button } from "@mui/material";
+import { GridColDef } from "@mui/x-data-grid-pro";
+import {
+  useGetAllDocuments,
+  useRestoreRevision,
+} from "../../../../hooks/document/documentHooks.ts";
+import { useCallback, useState } from "react";
+import { PaginationModel } from "../../../../consts/types.ts";
 import SettingsBackupRestoreIcon from "@mui/icons-material/SettingsBackupRestore";
-import {useQueryClient} from "@tanstack/react-query";
-import {GenericTable} from "../../../../components/genericTable/genericTable.tsx";
-import {COLUMNS} from "../../consts.tsx";
-import {DocumentType} from "../../../../api/documentAPI/types.ts";
+import { useQueryClient } from "@tanstack/react-query";
+import { GenericTable } from "../../../../components/genericTable/genericTable.tsx";
+import { COLUMNS } from "../../consts.tsx";
+import { DocumentType } from "../../../../api/documentAPI/types.ts";
 
-export const RevisionGroup = ({
-                                  revisionGroup,
-                                  rows,
-                                  setRows
-                              }: Props) => {
-    const queryClient = useQueryClient();
-    const [pagination, setPagination] = useState<PaginationModel>({
-        pageSize: 5,
-        page: 0,
-    });
+export const RevisionGroup = ({ revisionGroup, rows, setRows }: Props) => {
+  const queryClient = useQueryClient();
+  const [pagination, setPagination] = useState<PaginationModel>({
+    pageSize: 5,
+    page: 0,
+  });
 
-    const documentsQuery = useGetAllDocuments(
-        pagination,
-        {
-            isFinal: false,
-            revisionGroup,
-        },
-        ["tags", "tags.tag", "category", "secretLevel"],
-        "GetRevisionGroupDocuments"
-    );
+  const documentsQuery = useGetAllDocuments(
+    pagination,
+    {
+      isFinal: false,
+      revisionGroup,
+    },
+    ["tags", "tags.tag", "category", "secretLevel"],
+    "GetRevisionGroupDocuments"
+  );
 
-    const restoreRevision = useRestoreRevision();
+  const restoreRevision = useRestoreRevision();
 
-    const handleRestoreRevision = useCallback(
-        async (documentId: string) => {
-            const restoredDocument = documentsQuery?.data?.data.find(doc => doc.id === parseInt(documentId));
-            const mainDocumentIndex = rows.findIndex((doc: DocumentType) => doc.revisionGroup === revisionGroup);
+  const handleRestoreRevision = useCallback(
+    async (documentId: string) => {
+      const restoredDocument = documentsQuery?.data?.data.find(
+        (doc) => doc.id === parseInt(documentId)
+      );
+      const mainDocumentIndex = rows.findIndex(
+        (doc: DocumentType) => doc.revisionGroup === revisionGroup
+      );
 
-            if (!restoredDocument) return;
+      if (!restoredDocument) return;
 
-            const updatedRows = [...rows];
+      const updatedRows = [...rows];
 
-            // Replace main document with the restored revision
-            updatedRows[mainDocumentIndex] = {...restoredDocument, isFinal: true};
+      // Replace main document with the restored revision
+      updatedRows[mainDocumentIndex] = { ...restoredDocument, isFinal: true };
 
-            // Update the state
-            setRows(updatedRows);
+      // Update the state
+      setRows(updatedRows);
 
-            await restoreRevision.mutateAsync(documentId);
-            await documentsQuery.refetch();
-        },
-        [restoreRevision, queryClient, rows]
-    );
+      await restoreRevision.mutateAsync(documentId);
+      await documentsQuery.refetch();
+    },
+    [restoreRevision, queryClient, rows]
+  );
 
+  const ACTION_COLUMN: GridColDef = {
+    field: "action",
+    headerName: "",
+    width: 80,
+    align: "center",
+    cellClassName: "socialMedia",
+    renderCell: ({ row }) => (
+      <Button
+        variant="outlined"
+        onClick={() => handleRestoreRevision(row.id)}
+        disabled={restoreRevision.isLoading}
+      >
+        <SettingsBackupRestoreIcon />
+      </Button>
+    ),
+  };
 
-    const ACTION_COLUMN: GridColDef = {
-        field: "action",
-        headerName: "",
-        width: 80,
-        align: "center",
-        cellClassName: "socialMedia",
-        renderCell: ({row}) => (
-            <Button
-                variant="outlined"
-                onClick={() => handleRestoreRevision(row.id)}
-                disabled={restoreRevision.isLoading}
-            >
-                <SettingsBackupRestoreIcon/>
-            </Button>
-        ),
-    };
-
-    return (
-        <GenericTable
-            loading={documentsQuery.isLoading ?? false}
-            columns={[...COLUMNS, ACTION_COLUMN]}
-            rows={documentsQuery?.data?.data ?? []}
-            rowCount={documentsQuery?.data?.total ?? 0}
-            onPaginationModelChange={setPagination}
-            hideFooterPagination={true}
-            disableColumnMenu
-            disableColumnFilter
-            disableColumnSelector
-            sx={{
-                "& .MuiDataGrid-columnHeaders": {display: "none"},
-                "& .MuiDataGrid-footerContainer": {display: "none"},
-                border: "none",
-                backgroundColor: "transparent",
-                width: "auto",
-                height: "auto",
-                paddingLeft: 6,
-            }}
-        />
-    );
+  return (
+    <GenericTable
+      loading={documentsQuery.isLoading ?? false}
+      columns={[...COLUMNS, ACTION_COLUMN]}
+      rows={documentsQuery?.data?.data ?? []}
+      rowCount={documentsQuery?.data?.total ?? 0}
+      onPaginationModelChange={setPagination}
+      hideFooterPagination={true}
+      disableColumnMenu
+      disableColumnFilter
+      disableColumnSelector
+      sx={{
+        "& .MuiDataGrid-columnHeaders": { display: "none" },
+        "& .MuiDataGrid-footerContainer": { display: "none" },
+        border: "none",
+        backgroundColor: "transparent",
+        width: "auto",
+        height: "auto",
+        paddingLeft: "1.5%",
+      }}
+    />
+  );
 };
